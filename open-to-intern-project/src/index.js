f704f90473b8d1d5a9f54c5d56cc8ee4bbc3d85b
ROHIT KUMAR <98684090+yourstrulyrohit@users.noreply.github.com>
2022-03-24 03:24:19 +0530

    Update index.js

--- open-to-intern-project/src/index.js
+++ open-to-intern-project/src/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://rohankesarkar:rohan123@cluster0.sgev7.mongodb.net/openToInterns", {
+mongoose.connect("mongodb+srv://yourstrulyrohit:rohit123@cluster0.6iuya.mongodb.net/Group65Database?authSource=admin&replicaSet=atlas-w5j1a7-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
     useNewUrlParser: true
 })
     .then(() => console.log("MongoDb is connected"))

524e13f80ecbfb4de611cd61ca271f253a33391e
ROHIT <yourstrulyrohit@gmail.com>
2022-04-08 11:07:36 +0530

    project UrlShortner

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -70,8 +70,9 @@ const shortenUrl = async (req, res) => {
 //redirectToOriginalUrl....................................................................
 
 const getUrl = async (req, res) => {
-try {
-    const urlCode = req.params.urlCode.trim();
+// try {
+//     const urlCode = req.params.urlCode.trim();
+
 
     // if (Object.entries(urlCode)== 0||(urlCode)==null) {
     //     return res.status(400).send({ status: false, Message: "BAD REQUEST" });
@@ -85,24 +86,58 @@ try {
     //     }
     //     return res.status(302).redirect(isUrlExist.longUrl);
     // }
-        let getUrlCode= await GET_ASYNC(`${req.params.urlCode}`)
-        // if(!getUrlCode){res.send({msg:"enter valid url code"})}
+//         let getUrlCode= await GET_ASYNC(`${req.params.urlCode}`)
+//         // if(!getUrlCode){res.send({msg:"enter valid url code"})}
 
 
-        if(getUrlCode){
-            res.send(getUrlCode)
-            console.log(getUrlCode)
-            // if(!getUrlCode){res.send({msg:"enter valid url code"})}
-        }else{
-            let seturlCode=await urlModel.findOne({urlCode:urlCode})
-            await SET_ASYNC((`${req.params.urlCode}`),JSON.stringify(seturlCode))
-            res.send({data:seturlCode})
-console.log(seturlCode)
-    } 
-} catch (error) {
-    res.status(500).send({ status: false, Message: error.message });
+//         if(getUrlCode){
+//             res.send(getUrlCode)
+//             console.log(getUrlCode)
+//             // if(!getUrlCode){res.send({msg:"enter valid url code"})}
+//         }else{
+//             let seturlCode=await urlModel.findOne({urlCode:urlCode})
+//             await SET_ASYNC((`${req.params.urlCode}`),JSON.stringify(seturlCode))
+//             res.send({data:seturlCode})
+// console.log(seturlCode)
+//     } 
+// } catch (error) {
+//     res.status(500).send({ status: false, Message: error.message });
+// }
+// };
+try {
+    const urlCode = req.params.urlCode
+
+    // if (!validUrl.isUri(urlCode)) {
+    //     return res.status(400).send({ status: false, message: "please provide urlcode" })
+    // }
+    if(!urlCode){
+    if (typeof value == undefined || value == null || value == 0) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true
+    }
+
+    const cahcedOrginalUrl = await GET_ASYNC(`${urlCode}`)
+    if (cahcedOrginalUrl) {
+
+        let redirectingData = JSON.parse(cahcedOrginalUrl)
+
+        return res.status(302).redirect(redirectingData)
+    } else { 
+
+        const urlData = await urlModel.findOne({ urlCode: urlCode })
+        if (!urlData) {
+            return res.status(404).send({ status: false, message: "url not found" })
+    }
+
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData.longUrl))
+
+        return res.status(302).redirect(urlData.longUrl)
+
+    }
+} catch (err) {
+    return res.status(500).send({ status: false, message: err.message })
 }
-};
+}
 // module.exports.cachedCode=cachedCode
 module.exports.shortenUrl = shortenUrl;
 

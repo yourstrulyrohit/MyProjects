a1186856d92505ee4fe24ff8391572d2fde32cec
ROHIT KUMAR <98684090+yourstrulyrohit@users.noreply.github.com>
2022-05-19 03:27:55 +0530

    middleware

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,68 +1,34 @@
-const jwt = require('jsonwebtoken')
-const userModel = require("../models/userModel")
+const jwt = require("jsonwebtoken");
 
+const userAuth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization', 'Bearer ')
 
-//Authentication................................................................
-
-const authentication = function (req, res, next) {
-    try {
-        const token = req.headers["authorization"]
-        if (!token) {
-            return res.status(400).send({ status: false, message: "token must be present" });
-        }
-        const bearer=token.split(' ')
-        const bearerToken=bearer[1]
-        const decodedToken = jwt.verify(bearerToken, "Secret-Key");
-
-        if (!decodedToken) {
-            return res.status(400).send({ status: false, message: "token is invalid" });
-        }
-        //req.decodedToken=decodedToken
-        next();
-    }
-    catch (err) {
-        console.log(err)
-        return res.status(500).send({ msg: err.message })
+    if (!token) {
+      return res.status(403).send({
+          status: false,
+          message: `Missing authentication token in request`,
+        });
     }
 
-}
+    let splitToken = token.split(' ')
 
-
-//Authorization.....................................................................
-
-let authorization = async function (req, res, next) {
-
-    try {
-        let token = req.headers["authorization"]
-        if (!token) { return res.status(400).send({ status: false, message: "token must be present" }) }
-
-        const bearer=token.split(' ')
-        const bearerToken=bearer[1]
-        const decodedToken = jwt.verify(bearerToken, "Secret-Key");
-
-        if (!decodedToken) {
-            return res.status(400).send({ status: false, message: "token is invalid" });
-        }
-
-        let decodedUserId = decodedToken.userId
-        let userIdParams = req.params.userId
-
-        let userDetailsId = await userModel.findById({ _id: userIdParams })
-        if (!userDetailsId) {
-            return res.status(401).send({ status: false, msg: "no data found with this Id" });
-        }
-
-        let checkUserId = userDetailsId._id
-
-        if (decodedUserId != checkUserId) { return res.status(403).send({ status: false, message: "You are not an authorized person to make these changes" }) }
-        next()
+    const decodeToken = jwt.verify(splitToken[1], "Secret-Key")
+    
+    if (!decodeToken) {
+      return res.status(403).send({
+          status: false,
+          message: `Invalid authentication token in request`,
+        });
     }
-    catch (error) {
-        console.log(error)
-        return res.status(500).send({ msg: error.message })
-    }
-}
 
+    req.userId = decodeToken.userId
 
-module.exports.authorization = authorization
-module.exports.authentication = authentication
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
+
+module.exports.userAuth = userAuth;
